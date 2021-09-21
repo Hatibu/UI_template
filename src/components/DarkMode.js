6345af61c08a490a019d5395e715e5e10530b022
Hatibu <juliushatibu@gmail.com>
2021-09-21 10:56:53 +0300

    build:responsive navigation bar

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -5,7 +5,16 @@ const DarkMode = () => {
     const [nextTheme, setTheme] = useTheme();
     return (
         <div className="dark-mode">
-           
+           <div className="flex justify-center mt-10 bg-white dark:bg-black">
+                <button onClick={() => setTheme(nextTheme)} type="button" className="border-gray-200 bg-indigo-400 py-2 px-4 rounded-lg focus:outline-none ">
+                        Switch Theme
+                </button>
+
+                <div className="ml-5 text-black dark:text-white">Light mode and Dark mode</div>
+               
+               
+
+            </div>
         </div>
     )
 }
